2a65aa00de9817b3f560b4b0cdcf1c4276858540
Ademola <ademolaogunmokun@yahoo.com>
2022-06-03 17:15:42 +0100

    start afresh

--- src/App.js
+++ src/App.js
@@ -1,38 +1,20 @@
 
 import './App.css';
-import { AppStyle } from './components/styles/App.style';
+
 import { useState } from 'react';
 
-import { gameContext } from './Helper/Context';
-import Playcircle from './components/PlayCircle';
+
+
 
 
 function App() {
 
-  const [PlayState, SetPlayState] = useState({
-    PlayerX: "X",
-    PlayerY: "Y"
-  });
   
   
   
   return ( 
-    
-    
-    <AppStyle>
-    <gameContext.Provider value={{PlayState, SetPlayState}}>
-<Playcircle>
-
-</Playcircle>
-    </gameContext.Provider>
-
-
-  </AppStyle>
-
-
-      
-
-  
+  <>
+  </>  
     
   
   );
--- src/components/styles/cond.styles.js
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-export const Cond = styled.div`
-background-color: black;
-color: white;
-height: 50px;
-width: 60px;
-
-
-`
