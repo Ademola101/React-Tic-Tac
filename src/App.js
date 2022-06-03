10360b15eeb5012837273809f64c6ef529ea9956
Ademola <ademolaogunmokun@yahoo.com>
2022-06-03 17:31:08 +0100

    styling

--- src/App.js
+++ src/App.js
@@ -9,12 +9,16 @@ import { useState } from 'react';
 
 function App() {
 
-  
+  const [board,setBoard] = useState(Array(9).fill(null));
   
   
   return ( 
-  <>
-  </>  
+  <div clasName = "App">
+  <div className='Board'>
+
+
+  </div>
+  </div>  
     
   
   );
